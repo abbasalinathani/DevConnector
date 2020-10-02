687be99325d8f46e5c148413cf5d7ced68d493eb
Abbasali Nathani <abbasali.nathani@decos.com>
2020-10-02 12:05:08 +0530

    profile action, reducer

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -21,6 +21,9 @@ router.get('/me', auth, async (req, res) => {
     if (!profile) {
       return res.status(400).json({ msg: 'There is no profile for this user' });
     }
+
+    return res.json(profile);
+
   } catch (error) {
     console.error(error.message);
     res.status(500).send('Server error');
