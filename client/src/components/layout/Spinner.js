66e285048d56e5daab650a7c2b01ea02eee6ac80
Abbasali Nathani <abbasali.nathani@decos.com>
2020-10-06 21:42:35 +0530

    dashboard and spinner

---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,14 @@
+import React, { Fragment, Freagment } from 'react';
+import spinner from './spinner.gif';
+
+export default () => {
+  return (
+    <Fragment>
+      <img
+        src={spinner}
+        style={{ width: '200px', margin: 'auto', display: 'block' }}
+        alt='Loading...'
+      />
+    </Fragment>
+  );
+};
