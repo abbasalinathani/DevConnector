268bbff57926ada40c4b750df5ba9bfdba8e429f
Abbasali Nathani <abbasalinathani@hotmail.com>
2021-01-27 21:17:42 +0300

    display single post

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import Posts from './components/posts/Posts';
 import PrivateRoute from './components/routing/PrivateRoute';
+import Post from './components/post/Post';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -66,6 +67,7 @@ const App = () => {
                 component={AddEducation}
               />
               <PrivateRoute exact path='/posts' component={Posts} />
+              <PrivateRoute exact path='/post/:id' component={Post} />
             </Switch>
           </section>
         </Fragment>
